126d898f871e3d0939dce0e5dbe9dd6903c007c0
SH Hridoy <shhridoy.cste@gmail.com>
2017-08-16 01:23:05 +0600

    Create Class and Object.kt

---
+++ Class and Object.kt
@@ -0,0 +1,27 @@
+
+class student {
+    var name : String = "" // ALWAYS SHOULD ASSIGN A VALUE IF IT IS NOT THEN IT PRODUCE ERROR
+    var id : Int = 0
+    var session  = ""
+
+    fun assign(n : String, i : Int, s : String){
+        name = n
+        id = i
+        session = s
+    }
+
+    fun print(){
+        println("NAME: $name")
+        println("ID: $id")
+        println("SESSION: $session")
+    }
+}
+
+fun main(args : Array<String>){
+
+    var st = student() // CREATING OBJECT OF A CLASS
+    st.assign("Hridoy", 15010, "2014-2015") // PASSING THE ARGUMENTS
+    //st.assign(i = 15010, n = "Hridoy", s = "2014-2015") // PASS ARGUMENTS IN DISORDER
+    st.print()
+
+}
