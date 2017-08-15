1f7196f530ac6aaf3929458d70a766fc6baa6287
SH Hridoy <shhridoy.cste@gmail.com>
2017-08-16 02:05:43 +0600

    using JAVA library inside KOTLIN file

---
+++ Java inside Kotlin.kt
@@ -0,0 +1,17 @@
+
+fun main(args : Array<String>){
+
+    println("Hello! Kotlin")
+    System.out.println("Hello! Java")
+
+    println("5".toInt())
+    System.out.println(Integer.parseInt("5"))
+
+}
+
+/* OUTPUT:
+Hello! Kotlin
+Hello! Java
+5
+5
+*/
